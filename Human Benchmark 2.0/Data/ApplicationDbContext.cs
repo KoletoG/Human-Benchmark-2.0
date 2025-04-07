3e2e6c8f4cbded36da6499fa95e94d5d3bb6e6bd
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-07 20:28:30 +0300

    fix: Fixed reactiontimes[] not saving in database
    Added conversion for reactiontimes[] in OnModelCreating() so it can be saved in the database.

--- Human Benchmark 2.0/Data/ApplicationDbContext.cs
+++ Human Benchmark 2.0/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace Human_Benchmark_2._0.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserDataModel>().Property(x => x.reactionTimesArray).HasConversion(
+                x => string.Join(',', x),
+                x => x.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray()
+                );
+        }
     }
 }
