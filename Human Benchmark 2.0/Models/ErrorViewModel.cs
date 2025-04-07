09f476c0d361c5231eff746eac6d29380381aed6
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-07 08:30:50 +0300

    Add project files.

---
+++ Human Benchmark 2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Human_Benchmark_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
