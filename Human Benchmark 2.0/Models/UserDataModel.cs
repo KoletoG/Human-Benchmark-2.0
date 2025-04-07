d801863821cf1a667cdcf8cb4ce4dde2543e8bea
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-07 20:53:50 +0300

    fix: Fixed array elements not adding after index 0
    Removed currentIndexer as it wasn't working properly and added validation in AddReactionTimeToArray() for if the array contains any empty elements, that way it doesn't run the first loop every time.

--- Human Benchmark 2.0/Models/UserDataModel.cs
+++ Human Benchmark 2.0/Models/UserDataModel.cs
@@ -18,7 +18,6 @@ namespace Human_Benchmark_2._0.Models
         [Required]
         [DataType(DataType.EmailAddress)]
         public override string? Email { get; set; }
-        private int currentIndex = 0;
         private const int arrayCount = 5;
         public int[] reactionTimesArray = new int[arrayCount];
         public UserDataModel(string email, string username)
@@ -33,18 +32,24 @@ namespace Human_Benchmark_2._0.Models
         }
         public void AddReactionTimeToArray(int reaction)
         {
-            if (currentIndex > arrayCount-1)
+            if (reactionTimesArray.Contains(default))
             {
-                for(int i = 0; i < arrayCount-1; i++)
+                for (int i = 0; i < arrayCount; i++)
                 {
-                    this.reactionTimesArray[i] = this.reactionTimesArray[i + 1];
+                    if (reactionTimesArray[i] == default)
+                    {
+                        reactionTimesArray[i] = reaction;
+                        return;
+                    }
                 }
-                this.reactionTimesArray[arrayCount - 1] = reaction;
             }
             else
             {
-                this.reactionTimesArray[currentIndex] = reaction;
-                currentIndex++;
+                for (int i = 0; i < arrayCount - 1; i++)
+                {
+                    reactionTimesArray[i] = reactionTimesArray[i + 1];
+                }
+                reactionTimesArray[arrayCount - 1] = reaction;
             }
         }
     }
