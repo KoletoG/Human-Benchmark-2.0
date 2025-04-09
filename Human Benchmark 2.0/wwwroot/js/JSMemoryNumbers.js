df4c9fcfe7db26346b052badc251f12fbac18327
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-09 10:15:14 +0300

    chore: Created a separate file for Javascript
    Created a separate file for MemoryNumbers' javascript code.

---
+++ Human Benchmark 2.0/wwwroot/js/JSMemoryNumbers.js
@@ -0,0 +1,24 @@
+﻿let number = "";
+let currentLength = 1;
+
+for(let i =0;i<currentLength;i++)
+{
+    number[i]=Math.random()*10;
+}
+document.getElementById("numberField").style.color="black";
+document.getElementById("numberField").innerText=number;
+let answer = document.getElementById("answerField").innerText;
+function checkAnswer()
+{
+if(parseInt(number) == parseInt(answer))
+{
+    document.getElementById("numberField").style.color="green";
+    document.getElementById("numberField").innerText="Your answer is right. Continuing..."
+}
+else
+{
+    
+    document.getElementById("numberField").style.color="red";
+    document.getElementById("numberField").innerText="Your answer was wrong!";
+}
+}
