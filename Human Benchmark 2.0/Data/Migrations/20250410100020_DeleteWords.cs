5784c7739ec2190368b9d7e8c77bebae7ce8dd4d
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-10 13:09:15 +0300

    feat: Used an API to get words from MIT wordslist
    Used an API to get words list from MIT wordslist with 10000 words.

---
+++ Human Benchmark 2.0/Data/Migrations/20250410100020_DeleteWords.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Human_Benchmark_2._0.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteWords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "wordDataModels");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "wordDataModels",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Word = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wordDataModels", x => x.Id);
+                });
+        }
+    }
+}
--- Human Benchmark 2.0/wwwroot/js/JSMemoryWords.js
+++ Human Benchmark 2.0/wwwroot/js/JSMemoryWords.js
@@ -1,1 +1,33 @@
-﻿
+﻿let wordsAllList = ["word1","word2","word3","word4","word5"];
+let wordsCurrentList = [""];
+let wordChoice = "";
+let isTrueOrFalse = false;
+let response;
+document.addEventListener("DOMContentLoaded", () => 
+{
+    let currentWord = document.getElementById("currentWord");
+});
+async function startGame()
+{
+    await loadWords();
+    await grabWord();
+}
+async function grabWord()
+{
+    const text = await response.text();
+    const words = text.split("\n").filter(w => w.trim().length > 0);
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const randomWord = words[randomIndex];
+    currentWord.innerText = randomWord;
+}
+async function loadWords()
+{
+    try 
+    {
+        response = await fetch("https://www.mit.edu/~ecprice/wordlist.10000");
+    }
+    catch (error)
+    {
+        console.error("Error fetching word list:", error);
+    }
+}
