b3c5ff696008763d47d625d0c90f3a448ae34c73
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-10 12:23:28 +0300

    chore: Added MemoryWordsController, MemoryWordsMain view and JSMemoryWords
    Added basis of the MemoryWords mini-game

---
+++ Human Benchmark 2.0/Controllers/MemoryWordsController.cs
@@ -0,0 +1,21 @@
+﻿using Human_Benchmark_2._0.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Benchmark_2._0.Controllers
+{
+    public class MemoryWordsController : Controller
+    {
+        private readonly ILogger<MemoryWordsController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public MemoryWordsController(ILogger<MemoryWordsController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult MemoryWordsMain()
+        {
+            return View();
+        }
+    }
+}
