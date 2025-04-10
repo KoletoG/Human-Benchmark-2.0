6799c3575e229f7226f65acad9af2ebbe86c6669
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-10 17:18:56 +0300

    chore: Added View for MemoryWords and JSscript
    Added basic view of the MemoryWords and logic. Need to implement words list.

--- Human Benchmark 2.0/wwwroot/js/JSMemoryWords.js
+++ Human Benchmark 2.0/wwwroot/js/JSMemoryWords.js
@@ -1,33 +1,73 @@
-﻿let wordsAllList = ["word1","word2","word3","word4","word5"];
-let wordsCurrentList = [""];
-let wordChoice = "";
-let isTrueOrFalse = false;
-let response;
-document.addEventListener("DOMContentLoaded", () => 
-{
-    let currentWord = document.getElementById("currentWord");
-});
+﻿let wordsCurrentList = [];
+let score=0;
+let randomWord="";
+
 async function startGame()
 {
-    await loadWords();
     await grabWord();
+    document.getElementById("seenBtn").hidden = false;
+    document.getElementById("seenBtn").disabled = false;
+    document.getElementById("notSeenBtn").hidden = false;
+    document.getElementById("notSeenBtn").disabled = false;
 }
+
+async function seenWord()
+{
+    if(wordsCurrentList.includes(randomWord))
+    {
+        score++;
+        await grabWord();
+    }
+    else
+    {
+        gameOver();
+    }
+}
+
+function gameOver()
+{
+    document.getElementById("seenBtn").hidden = true;
+    document.getElementById("seenBtn").disabled = true;
+    document.getElementById("notSeenBtn").hidden = true;
+    document.getElementById("notSeenBtn").disabled = true;
+    document.getElementById("saveBtn").hidden = false;
+    document.getElementById("saveBtn").disabled = false;
+    document.getElementById("restartBtn").hidden=false;
+    document.getElementById("restartBtn").disabled=false;
+    document.getElementById("currentWord").innerText=score;
+}
+
+function restartGame()
+{
+    document.getElementById("saveBtn").hidden = true;
+    document.getElementById("saveBtn").disabled = true;
+    document.getElementById("restartBtn").hidden=true;
+    document.getElementById("restartBtn").disabled=true;
+    score=0;
+    wordsCurrentList=[];
+    startGame();
+}
+
+async function notSeenWord()
+{
+    if(wordsCurrentList.includes(randomWord))
+    {
+        gameOver();
+    }
+    else
+    {
+        score++;
+        wordsCurrentList.push(randomWord);
+        await grabWord();
+    }
+}
+
 async function grabWord()
 {
-    const text = await response.text();
+    let response = await fetch("https://wordsapiv1.p.mashape.com/words/example");
+    const text = response.text();
     const words = text.split("\n").filter(w => w.trim().length > 0);
     const randomIndex = Math.floor(Math.random() * words.length);
-    const randomWord = words[randomIndex];
-    currentWord.innerText = randomWord;
-}
-async function loadWords()
-{
-    try 
-    {
-        response = await fetch("https://www.mit.edu/~ecprice/wordlist.10000");
-    }
-    catch (error)
-    {
-        console.error("Error fetching word list:", error);
-    }
+    randomWord = words[randomIndex];
+    document.getElementById("currentWord").innerText = randomWord;
 }
