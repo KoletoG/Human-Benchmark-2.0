c5f25e0e6347c1a817fcb6298b9a3262a9d2d104
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-11 09:22:33 +0300

    chore: Added ReverseWord structure
    Added ReverseWordController and view, added the view to _Layout.

---
+++ Human Benchmark 2.0/Controllers/ReverseWordController.cs
@@ -0,0 +1,21 @@
+﻿using Human_Benchmark_2._0.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Benchmark_2._0.Controllers
+{
+    public class ReverseWordController : Controller
+    {
+        private readonly ILogger<ReverseWordController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ReverseWordController(ILogger<ReverseWordController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult ReverseWordMain()
+        {
+            return View();
+        }
+    }
+}
