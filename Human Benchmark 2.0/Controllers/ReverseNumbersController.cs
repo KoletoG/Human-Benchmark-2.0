0d35aeee65763993a999c167171207393d92b74d
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-11 15:06:34 +0300

    chore: Added foundation of ReverseNumbers mini-game
    Added controller, view and JS file for ReverseNumbers. Added View to layout.

---
+++ Human Benchmark 2.0/Controllers/ReverseNumbersController.cs
@@ -0,0 +1,34 @@
+﻿using Human_Benchmark_2._0.Data;
+using Human_Benchmark_2._0.Methods;
+using Human_Benchmark_2._0.Models.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Benchmark_2._0.Controllers
+{
+    public class ReverseNumbersController : Controller
+    {
+        private readonly ILogger<ReverseNumbersController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ReverseNumbersController(ILogger<ReverseNumbersController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult ReverseNumbersMain()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> SaveWordsScore([FromBody] int score)
+        {
+            UserDataModel userDataModel = await _context.GetUserByNameAsync(this.User.Identity?.Name ?? "");
+            userDataModel.AddReverseWordsScoreToArray(score);
+            _context.Update(userDataModel);
+            _context.SaveChanges();
+            return Json(new { redirectUrl = Url.Action("Profile", "Home") });
+        }
+    }
+}
