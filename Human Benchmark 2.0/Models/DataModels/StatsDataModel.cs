50e71d1c144891bb85bf1463fa4da9190a4a0d49
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-12 17:42:51 +0300

    chore: Deleted StatsDataModel
    Deleted StatsDataModel as it was useless

--- Human Benchmark 2.0/Models/DataModels/StatsDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Human_Benchmark_2._0.Models.DataModels
-{
-    public class StatsDataModel
-    {
-            public double AvgTime { get; set; }
-            public int CorrectAnswers { get; set; }
-        
-    }
-}
