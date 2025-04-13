2c727d1d896d14186e3cf66b35885fd3f5a5a917
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-13 08:36:35 +0300

    chore: Added foundation for AudioReaction mini-game
    Added controller, view, JS script file and registered it in the _Layout.

---
+++ Human Benchmark 2.0/Controllers/AudioReactionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Benchmark_2._0.Controllers
+{
+    public class AudioReactionController : Controller
+    {
+        public IActionResult AudioReactionMain()
+        {
+            return View();
+        }
+    }
+}
