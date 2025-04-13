c20e3a9ebaef631105af616e7f41f97978691ea9
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-13 23:18:28 +0300

    refactor: Added WordDataModel
    Added class WordDataModel and added it into the database

---
+++ Human Benchmark 2.0/Models/DataModels/WordDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Human_Benchmark_2._0.Models.DataModels
+{
+    public class WordDataModel
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Word { get; set; }
+        public WordDataModel(string word) 
+        {
+            Id=Guid.NewGuid().ToString();
+            Word = word;
+        }
+    }
+}
