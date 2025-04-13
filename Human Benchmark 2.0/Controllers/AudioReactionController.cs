ba785f0a8b7192b6b62992c3ccb54f2bd893c713
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-13 08:39:03 +0300

    chore: Added logger and database access for audioController
    Added Logger<AudioReactionController> and AplicationDbContext instances for audioController.

--- Human Benchmark 2.0/Controllers/AudioReactionController.cs
+++ Human Benchmark 2.0/Controllers/AudioReactionController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Human_Benchmark_2._0.Data;
+using Human_Benchmark_2._0.Methods;
+using Human_Benchmark_2._0.Models.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Human_Benchmark_2._0.Controllers
 {
     public class AudioReactionController : Controller
     {
+        private readonly ILogger<AudioReactionController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public AudioReactionController(ILogger<AudioReactionController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        [Authorize]
         public IActionResult AudioReactionMain()
         {
             return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SaveAudioScore([FromBody] int score)
+        {
+            UserDataModel userDataModel = await _context.GetUserByNameAsync(this.User.Identity?.Name ?? "");
+            userDataModel.AddReverseWordsScoreToArray(score);
+            _context.Update(userDataModel);
+            _context.SaveChanges();
+            return Json(new { redirectUrl = Url.Action("Profile", "Home") });
         }
     }
 }
