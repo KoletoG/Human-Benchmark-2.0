dd4641d2f2dbdb01aafee70c6a49f445ece923d6
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-16 17:49:22 +0300

    feat: Created basic structure of new Error view page
    Created a view and a viewModel for ThrownException page

---
+++ Human Benchmark 2.0/Models/ViewModels/ThrownExceptionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Human_Benchmark_2._0.Models.ViewModels
+{
+    public class ThrownExceptionViewModel
+    {
+        public string Message { get; set; }
+        public Exception ExceptionM { get; set; }
+        public ThrownExceptionViewModel(string message, Exception exception)
+        {
+            Message = message;
+            ExceptionM = exception;
+        }
+    }
+}
