35d9d8fffae748ed5cd4f07b1e194bf5dc86dce1
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-16 09:27:22 +0300

    feat: Added basic foundation of InterestingQuestions mini-game
    Added controller, view, JS file for InterestingQuestions mini-game and added it to the _Layout.

---
+++ Human Benchmark 2.0/Controllers/InterestingQuestionsController.cs
@@ -0,0 +1,21 @@
+﻿using Human_Benchmark_2._0.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Human_Benchmark_2._0.Controllers
+{
+    public class InterestingQuestionsController : Controller
+    {
+        private readonly ILogger<InterestingQuestionsController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public InterestingQuestionsController(ILogger<InterestingQuestionsController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult InterestingQuestionsMain()
+        {
+            return View();
+        }
+    }
+}
